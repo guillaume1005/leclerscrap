ff47a429066c64127a7efefda85e98770efa867e
KERVELLA <guillaumekervella1@gmail.com>
2021-09-30 10:40:51 +0200

    boss

--- src/App.js
+++ src/App.js
@@ -24,23 +24,73 @@ function App() {
 
   return (
     <div className="App">
-      <Button variant="contained">SALUT BIG BOSS KERVELLA</Button>
-      {obj.map(el => {
+      <Button variant="contained" style={{ margin: 30 }}>
+        SCRAP LECLERC
+      </Button>
+      <div style={{display: "flex", alignItems: "center", marginLeft: 400, marginRight: 400}}>
+        <div>
+          {" "}
+          Le "Pourcentage Première Page" est le ratio de présence en première
+          page, c'est lui qui est important. Le "Pourcentage Marché" est le
+          ratio du nombre d'offres sur le marché total (toutes les pages). Le
+          site est mis à jour tous les jours entre 9h 30 et 9h 45.
+        </div>
+      </div>
+      {obj.map((el) => {
         return (
-          <div style={{display: 'flex', alignItems: "center", flexDirection: "row", justifyContent: "center", marginTop: 10}}>
-
-            <div style={{border: "solid", width: 250, alignItems: "center", justifyContent: 'center', padding: 10, marginLeft: 10}}>Pourcentage Market: {el.percentageMarket}</div>
-            <div style={{border: "solid", width: 250, alignItems: "center", justifyContent: 'center', padding: 10, marginLeft: 10}}>Desti: {el.desti}</div>
-
-            <div style={{border: "solid", width: 250, alignItems: "center", justifyContent: "center", padding: 10, marginLeft: 10, display: 'flex', flexDirection: 'row', color: "green"}}>
-              <div style={{color: "green"}}>Pourcentage FirstPage: </div>
-              <div style={{color: "green"}}>{el.percentageFirstPage}</div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "row",
+              justifyContent: "center",
+              marginTop: 10,
+            }}
+          >
+            <div
+              style={{
+                border: "solid",
+                width: 250,
+                alignItems: "center",
+                justifyContent: "center",
+                padding: 10,
+                marginLeft: 10,
+              }}
+            >
+              Pourcentage Marché: {el.percentageMarket}
             </div>
-            
+            <div
+              style={{
+                border: "solid",
+                width: 250,
+                alignItems: "center",
+                justifyContent: "center",
+                padding: 10,
+                marginLeft: 10,
+              }}
+            >
+              Desti: {el.desti}
+            </div>
+
+            <div
+              style={{
+                border: "solid",
+                width: 250,
+                alignItems: "center",
+                justifyContent: "center",
+                padding: 10,
+                marginLeft: 10,
+                display: "flex",
+                flexDirection: "row",
+                color: "green",
+              }}
+            >
+              <div style={{ color: "green" }}>Pourcentage Première Page: </div>
+              <div style={{ color: "green" }}>{el.percentageFirstPage}</div>
+            </div>
           </div>
         );
-      }
-      )}
+      })}
     </div>
   );
 }
